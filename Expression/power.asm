4eb2be541b130f0e011090a09b8ebf5dda231f01
AbbasCodex <142521866+AbbasCodex@users.noreply.github.com>
2024-03-16 17:44:58 +0530

    Update power.asm

--- Expression/power.asm
+++ Expression/power.asm
@@ -1,59 +1,92 @@
-; Code for Program to Calculate power(a,b) i.e a^b in Assembly Language
 .MODEL SMALL
 .DATA
-        BASE DB ?
-        POW DB ?
-        NL1 DB 0AH,0DH,'ENTER BASE:','$'
-        NL2 DB 0AH,0DH,'ENTER POWER:','$'
+    BASE DW ?
+    POW DB ?
+    NL1 DB 0AH,0DH,'ENTER BASE: ','$'
+    NL2 DB 0AH,0DH,'ENTER POWER: ','$'
+    RESULT DB 6 DUP('$')
 
 .CODE
 MAIN PROC
 
-        MOV AX,@DATA
-        MOV DS,AX
+    MOV AX,@DATA
+    MOV DS,AX
+
+    CALL ENTER_BASE
+    CALL ENTER_POWER
+
+    MOV AX, BASE
+    MOV BX, POW
+    CALL POWER
+
+    MOV AH, 09H     ; Print result
+    LEA DX, RESULT
+    INT 21H
+
+    MOV AH,4CH      ; Exit program
+    INT 21H
 
 ENTER_BASE:
-        LEA DX,NL1
-        MOV AH,09H
-        INT 21H
+    LEA DX, NL1     ; Display prompt
+    MOV AH, 09H
+    INT 21H
 
-        MOV AH,01H
-        INT 21H
-        SUB AL,30H
-        MOV BL,AL
-
-        MOV BASE,AL
+    CALL READ_NUMBER ; Read base
+    MOV BASE, AX
+    RET
 
 ENTER_POWER:
-        LEA DX,NL2
-        MOV AH,09H
-        INT 21H
+    LEA DX, NL2     ; Display prompt
+    MOV AH, 09H
+    INT 21H
 
-        MOV AH,01H
-        INT 21H
-        SUB AL,30H
+    CALL READ_NUMBER ; Read power
+    MOV POW, AL
+    RET
 
-        MOV CL,AL
-        DEC CL
-        MOV AX,00
-        MOV AL,BASE
+READ_NUMBER:
+    XOR AX, AX      ; Clear AX
+    MOV CX, 10      ; Multiplier
+READ_LOOP:
+    MOV AH, 01H     ; Read a character
+    INT 21H
 
-LBL1:
-        MUL BL
-        LOOP LBL1
+    CMP AL, 0DH     ; Check for Enter key
+    JE DONE_READ    ; If Enter, finish reading
 
-        MOV CL,10
-        DIV CL
-        ADD AX,3030H
-        MOV DX,AX
+    SUB AL, 30H     ; Convert ASCII to digit
+    MUL CX          ; Multiply current number by 10
+    ADD AX, DX      ; Add new digit
+    MOV DX, AX      ; Store result
 
-        MOV AH,02H
-        INT 21H
-        MOV DL,DH
-        INT 21H
+    JMP READ_LOOP   ; Repeat
 
-        MOV AH,4CH
-        INT 21H
+DONE_READ:
+    RET
+
+POWER PROC
+    MOV CX, POW     ; Move power to CX
+    MOV DX, 0       ; Clear DX (result)
+    MOV AX, 1       ; Set AX to 1 (base case)
+
+POWER_LOOP:
+    TEST CX, 1      ; Check if power is odd
+    JZ EVEN_POWER   ; If not odd, skip
+
+    MUL BX          ; Multiply result by base
+    DEC CX          ; Decrement power
+
+EVEN_POWER:
+    SHR CX, 1       ; Divide power by 2
+    JZ POWER_DONE   ; If power is zero, finish
+
+    MUL BX          ; Multiply base by itself
+    JMP POWER_LOOP  ; Repeat
+
+POWER_DONE:
+    MOV AX, DX      ; Move result to AX
+    RET
+POWER ENDP
 
 MAIN ENDP
 END MAIN
